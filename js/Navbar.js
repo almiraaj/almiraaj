0a46e321f2c9cbc7547528b519fcd0866ac3d42d
almiraaj <117502704+almiraaj@users.noreply.github.com>
2022-11-05 12:11:10 +0530

    Add files via upload

---
+++ js/Navbar.js
@@ -0,0 +1,411 @@
+var navbra = `<nav class="navbar navbar-expand-md navbar-dark navbar-custom fixed-top">
+<!-- Text Logo - Use this if you don't have a graphic logo -->
+<!-- <a class="navbar-brand logo-text page-scroll" href="index.html">Aria</a> -->
+
+<!-- Image Logo -->
+<a class="navbar-brand logo-image" href="index.html"
+  ><h3 class="logo-image">AL-MIRAAJ International</h3></a
+>
+
+<!-- Mobile Menu Toggle Button -->
+<button
+  class="navbar-toggler"
+  type="button"
+  data-toggle="collapse"
+  data-target="#navbarsExampleDefault"
+  aria-controls="navbarsExampleDefault"
+  aria-expanded="false"
+  aria-label="Toggle navigation"
+>
+  <span class="navbar-toggler-awesome fas fa-bars"></span>
+  <span class="navbar-toggler-awesome fas fa-times"></span>
+</button>
+<!-- end of mobile menu toggle button -->
+
+<div class="collapse navbar-collapse" id="navbarsExampleDefault">
+  <ul class="navbar-nav ml-auto">
+    <li class="nav-item">
+      <a class="nav-link page-scroll" href="#header"
+        >HOME <span class="sr-only">(current)</span></a
+      >
+    </li>
+    <li class="nav-item">
+      <a class="nav-link page-scroll" href="#intro">INTRO</a>
+    </li>
+   
+    <li class="nav-item dropdown">
+      <a
+        class="nav-link dropdown-toggle page-scroll "
+        id="navbarDropdown"
+        aria-haspopup="true"
+        aria-expanded="false"
+        >Certificate Attestation</a
+      >
+      <ul id="myMenun2" class="dropdown-menu"  aria-labelledby="navbarDropdown" >
+          <div>
+            <a
+              class="dropdown-item"
+              href="certificates/birth-certificate-attestation.html"
+              title="Birth Certificate Attestation"
+              ><span>Birth Certificate Attestation</span></a
+            >
+          </div>
+          <div class="dropdown-items-divide-hr"></div>
+          <div>
+            <a
+              class="dropdown-item"
+              href="certificates/marriage_certificate_attestation_42.html"
+              title="Marriage Certificate Attestation"
+              ><span>Marriage Certificate Attestation</span></a
+            >
+          </div>
+          <div class="dropdown-items-divide-hr"></div>
+          <div>
+            <a
+              class="dropdown-item"
+              href="certificates/educational_certificate_attestation_45.html"
+              title="Educational Certificate Attestation"
+              ><span>Educational Certificate Attestation</span></a
+            >
+          </div>
+          <div class="dropdown-items-divide-hr"></div>
+          <div>
+            <a
+              class="dropdown-item"
+              href="certificates/page_46.html"
+              title="Single Status Certificate Apostille / Attestation"
+              ><span
+                >Single Status Certificate Apostille / Attestation</span
+              ></a
+            >
+          </div>
+          <div class="dropdown-items-divide-hr"></div>
+          <div>
+            <a
+              class="dropdown-item"
+              href="certificates/how-to-get-single-status-certificate-in-chennai.html"
+              title="how to get single status certificate in Chennai"
+              ><span
+                >how to get single status certificate in Chennai</span
+              ></a
+            >
+          </div>
+          <div class="dropdown-items-divide-hr"></div>
+          <div>
+            <a
+              class="dropdown-item"
+              href="certificates/commercial-documents-attestation.html"
+              title="Commercial Documents Attestation"
+              ><span>Commercial Documents Attestation</span></a
+            >
+          </div>
+          <div class="dropdown-items-divide-hr"></div>
+          <div>
+            <a
+              class="dropdown-item"
+              href="certificates/power-of-attorney-apostilleattestation-in-chennai.html"
+              title="Power of Attorney Apostille/Attestation in Chennai"
+              ><span
+                >Power of Attorney Apostille/Attestation in Chennai</span
+              ></a
+            >
+          </div>
+          <div class="dropdown-items-divide-hr"></div>
+          <div>
+            <a
+              class="dropdown-item"
+              href="certificates/embassy-attestation.html"
+              title="Embassy Attestation"
+              ><span>Embassy Attestation</span></a
+            >
+          </div>
+          <div class="dropdown-items-divide-hr"></div>
+          <div>
+            <a
+              class="dropdown-item"
+              href="certificates/embassy-attestation-in-chennai-uaekuwaitqatar.html"
+              title="Embassy Attestation IN Chennai - UAE,Kuwait,Qatar"
+              ><span
+                >Embassy Attestation IN Chennai - UAE,Kuwait,Qatar</span
+              ></a
+            >
+          </div>
+          <div class="dropdown-items-divide-hr"></div>
+          <div>
+            <a
+              class="dropdown-item"
+              href="certificates/bahrain-embassy-attestation.html"
+              title="Bahrain Embassy Attestation"
+              ><span>Bahrain Embassy Attestation</span></a
+            >
+          </div>
+          <div class="dropdown-items-divide-hr"></div>
+          <div>
+            <a
+              class="dropdown-item"
+              href="certificates/china-embassy-attestation.html"
+              title="China Embassy Attestation"
+              ><span>China Embassy Attestation</span></a
+            >
+          </div>
+          <div class="dropdown-items-divide-hr"></div>
+          <div>
+            <a
+              class="dropdown-item"
+              href="certificates/egypt-embassy-attestation.html"
+              title="Egypt Embassy Attestation"
+              ><span>Egypt Embassy Attestation</span></a
+            >
+          </div>
+          <div class="dropdown-items-divide-hr"></div>
+          <div>
+            <a
+              class="dropdown-item"
+              href="certificates/kuwait-embassy-attestation.html"
+              title="Kuwait Embassy Attestation"
+              ><span>Kuwait Embassy Attestation</span></a
+            >
+          </div>
+          <div class="dropdown-items-divide-hr"></div>
+          <div>
+            <a
+              class="dropdown-item"
+              href="certificates/kuwait-embassy-attestation-procedures-in-chennai.html"
+              title="Kuwait Embassy Attestation Procedures in Chennai"
+              ><span
+                >Kuwait Embassy Attestation Procedures in Chennai</span
+              ></a
+            >
+          </div>
+          <div class="dropdown-items-divide-hr"></div>
+          <div>
+            <a
+              class="dropdown-item"
+              href="certificates/kuwait-embassy-attestation-in-tirunelveli.html"
+              title="Kuwait Embassy Attestation in Tirunelveli"
+              ><span>Kuwait Embassy Attestation in Tirunelveli</span></a
+            >
+          </div>
+          <div class="dropdown-items-divide-hr"></div>
+          <div>
+            <a
+              class="dropdown-item"
+              href="certificates/kuwait-embassy-attestation-in-coimbatore.html"
+              title="Kuwait Embassy Attestation in Coimbatore"
+              ><span>Kuwait Embassy Attestation in Coimbatore</span></a
+            >
+          </div>
+          <div class="dropdown-items-divide-hr"></div>
+          <div>
+            <a
+              class="dropdown-item"
+              href="certificates/kuwait-embassy-attestation-in-trichy.html"
+              title="Kuwait Embassy Attestation in Trichy"
+              ><span>Kuwait Embassy Attestation in Trichy</span></a
+            >
+          </div>
+          <div class="dropdown-items-divide-hr"></div>
+          <div>
+            <a
+              class="dropdown-item"
+              href="certificates/malaysia-embassy-attestation.html"
+              title="Malaysia Embassy Attestation"
+              ><span>Malaysia Embassy Attestation</span></a
+            >
+          </div>
+          <div class="dropdown-items-divide-hr"></div>
+          <div>
+            <a
+              class="dropdown-item"
+              href="certificates/oman-embassy-attestation.html"
+              title="Oman Embassy Attestation"
+              ><span>Oman Embassy Attestation</span></a
+            >
+          </div>
+          <div class="dropdown-items-divide-hr"></div>
+          <div>
+            <a
+              class="dropdown-item"
+              href="certificates/qatar-embassy-attestation.html"
+              title="Qatar Embassy Attestation"
+              ><span>Qatar Embassy Attestation</span></a
+            >
+          </div>
+          <div class="dropdown-items-divide-hr"></div>
+          <div>
+            <a
+              class="dropdown-item"
+              href="certificates/saudi-embassy-attestation.html"
+              title="Saudi Embassy Attestation"
+              ><span>Saudi Embassy Attestation</span></a
+            >
+          </div>
+          <div class="dropdown-items-divide-hr"></div>
+          <div>
+            <a
+              class="dropdown-item"
+              href="certificates/uae-embassy-attestation.html"
+              title="UAE Embassy Attestation"
+              ><span>UAE Embassy Attestation</span></a
+            >
+          </div>
+          <div class="dropdown-items-divide-hr"></div>
+          <div>
+            <a
+              class="dropdown-item"
+              href="certificates/process-of-uae-embassy-attestation-in-chennai.html"
+              title="Process of UAE Embassy Attestation in Chennai"
+              ><span
+                >Process of UAE Embassy Attestation in Chennai</span
+              ></a
+            >
+          </div>
+          <div class="dropdown-items-divide-hr"></div>
+          <div>
+            <a
+              class="dropdown-item"
+              href="certificates/uae-embassy-attestation-in-trichy.html"
+              title="UAE Embassy Attestation In Trichy"
+              ><span>UAE Embassy Attestation In Trichy</span></a
+            >
+          </div>
+          <div class="dropdown-items-divide-hr"></div>
+          <div>
+            <a
+              class="dropdown-item"
+              href="certificates/uae-embassy-attestation-in-coimbatore.html"
+              title="UAE Embassy Attestation In Coimbatore"
+              ><span>UAE Embassy Attestation In Coimbatore</span></a
+            >
+          </div>
+          <div class="dropdown-items-divide-hr"></div>
+          <div>
+            <a
+              class="dropdown-item"
+              href="certificates/uae-embassy-attestation-in-nagercoil.html"
+              title="UAE Embassy Attestation In Nagercoil"
+              ><span>UAE Embassy Attestation In Nagercoil</span></a
+            >
+          </div>
+          <div class="dropdown-items-divide-hr"></div>
+          <div>
+            <a
+              class="dropdown-item"
+              href="certificates/uae-embassy-attestation-in-tirunelveli.html"
+              title="UAE Embassy Attestation In Tirunelveli"
+              ><span>UAE Embassy Attestation In Tirunelveli</span></a
+            >
+          </div>
+          <div class="dropdown-items-divide-hr"></div>
+          <div>
+            <a
+              class="dropdown-item"
+              href="certificates/uae-embassy-attestation-in-tuticorin.html"
+              title="UAE Embassy Attestation In Tuticorin"
+              ><span>UAE Embassy Attestation In Tuticorin</span></a
+            >
+          </div>
+          <div class="dropdown-items-divide-hr"></div>
+          <div>
+            <a
+              class="dropdown-item"
+              href="certificates/uae-embassy-attestation-in-kanyakumari.html"
+              title="UAE Embassy Attestation In Kanyakumari"
+              ><span>UAE Embassy Attestation In Kanyakumari</span></a
+            >
+          </div>
+          <div class="dropdown-items-divide-hr"></div>
+          <div>
+            <a
+              class="dropdown-item"
+              href="certificates/hrd-attestation-process.html"
+              title="HRD Attestation Process"
+              ><span>HRD Attestation Process</span></a
+            >
+          </div>
+          <div class="dropdown-items-divide-hr"></div>
+          <div>
+            <a
+              class="dropdown-item"
+              href="certificates/home-mantralaya-attestation.html"
+              title="Home-Mantralaya Attestation"
+              ><span>Home-Mantralaya Attestation</span></a
+            >
+          </div>
+          <div class="dropdown-items-divide-hr"></div>
+        </ul>
+    </li>
+
+    <li class="nav-item dropdown">
+      <a
+        class="nav-link dropdown-toggle page-scroll"
+        id="navbarDropdown"
+        role="button"
+        aria-haspopup="true"
+        aria-expanded="false"
+        >Apostille MEA</a
+      >
+      <ul id="myMenu2" class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                              
+          <li><a class="dropdown-item" href="apostille/apostille-mea-attestation-in-chennai.html" title="Apostille MEA Attestation IN Chennai"><span>Apostille MEA Attestation IN Chennai</span></a></li>
+                 <div class="dropdown-items-divide-hr"></div>						
+          <li><a class="dropdown-item" href="apostille/apostille-mea-attestation-in-trichy.html" title="Apostille MEA Attestation in Trichy"><span>Apostille MEA Attestation in Trichy</span></a></li>
+                 <div class="dropdown-items-divide-hr"></div>						
+          <li><a class="dropdown-item" href="apostille/apostille-mea-attestation-in-coimbatore.html" title="Apostille MEA Attestation in Coimbatore"><span>Apostille MEA Attestation in Coimbatore</span></a></li>
+                 <div class="dropdown-items-divide-hr"></div>						
+          <li><a class="dropdown-item" href="apostille/apostille-mea-attestation-in-tirunelveli.html" title="Apostille MEA Attestation in Tirunelveli"><span>Apostille MEA Attestation in Tirunelveli</span></a></li>
+                 <div class="dropdown-items-divide-hr"></div>						
+          <li><a class="dropdown-item" href="apostille/apostille-mea-attestation-in-tuticorin.html" title="Apostille MEA Attestation in Tuticorin"><span>Apostille MEA Attestation in Tuticorin</span></a></li>
+                 <div class="dropdown-items-divide-hr"></div>						
+          <li><a class="dropdown-item" href="apostille/apostille-mea-attestation-in-nagercoil.html" title="Apostille MEA Attestation in Nagercoil"><span>Apostille MEA Attestation in Nagercoil</span></a></li>
+                 <div class="dropdown-items-divide-hr"></div>						
+          <li><a class="dropdown-item" href="apostille/apostille-mea-attestation-in-pondicherry.html" title="Apostille MEA Attestation in Pondicherry"><span>Apostille MEA Attestation in Pondicherry</span></a></li>
+                 <div class="dropdown-items-divide-hr"></div>						
+          <li><a class="dropdown-item" href="apostille/apostille-mea-attestation-in-karaikal.html" title="Apostille MEA Attestation in Karaikal"><span>Apostille MEA Attestation in Karaikal</span></a></li>
+          
+              </ul>
+    </li>
+
+    <!-- Dropdown Menu -->
+    <li class="nav-item dropdown">
+      <a
+        class="nav-link dropdown-toggle page-scroll"
+        href="#about"
+        id="navbarDropdown"
+        role="button"
+        aria-haspopup="true"
+        aria-expanded="false"
+        >ABOUT</a
+      >
+      <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+        <a class="dropdown-item" href="apostille/terms-conditions.html"
+          ><span class="item-text">TERMS CONDITIONS</span></a
+        >
+        <div class="dropdown-items-divide-hr"></div>
+        <a class="dropdown-item" href="apostille/privacy-policy.html"
+          ><span class="item-text">PRIVACY POLICY</span></a
+        >
+      </div>
+    </li>
+    <!-- end of dropdown menu -->
+
+    <li class="nav-item">
+      <a class="nav-link page-scroll" href="#contact">CONTACT</a>
+    </li>
+  </ul>
+  <span class="nav-item social-icons">
+    <span class="fa-stack">
+      <a href="https://www.facebook.com/AttestationAndApostille">
+        <span class="hexagon"></span>
+        <i class="fab fa-facebook-f fa-stack-1x"></i>
+      </a>
+    </span>
+    <span class="fa-stack">
+      <a href="https://www.facebook.com/AttestationAndApostille">
+        <span class="hexagon"></span>
+        <i class="fab fa-twitter fa-stack-1x"></i>
+      </a>
+    </span>
+  </span>
+</div>
+</nav>`
